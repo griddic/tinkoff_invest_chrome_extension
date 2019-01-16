637e39b960ad4460a32e77b7c32722e8d791c35e
gkoksharov <gkoksharov@ozon.travel>
2019-01-17 01:13:52 +0300

    load functions to body wait for a header before creating a table refresh button added

--- compute_and_insert_functions.js
+++ compute_and_insert_functions.js
@@ -46,7 +46,7 @@ function extract_currency_symbol(money_str) {
 }
 
 
-function compute_cost_martix(){
+function compute_cost_matrix(){
     var cost_by_type_and_currency = {
         stock: {
             rub: 0,
@@ -91,23 +91,23 @@ function compute_cost_martix(){
     }
     return cost_by_type_and_currency    
 }
-function compute_agregated(cost_matrix) {
-    var in_rubles = cost_by_type_and_currency['bond'][rub]
-    in_rubles = in_rubles + cost_by_type_and_currency['stock'][rub]
-    in_rubles = in_rubles + cost_by_type_and_currency['cash'][rub]
+function compute_aggregated(cost_matrix) {
+    var in_rubles = cost_matrix['bond'][rub]
+    in_rubles = in_rubles + cost_matrix['stock'][rub]
+    in_rubles = in_rubles + cost_matrix['cash'][rub]
 
-    var in_usd = cost_by_type_and_currency['bond'][usd]
-    in_usd = in_usd + cost_by_type_and_currency['stock'][usd]
-    in_usd = in_usd + cost_by_type_and_currency['cash'][usd]
+    var in_usd = cost_matrix['bond'][usd]
+    in_usd = in_usd + cost_matrix['stock'][usd]
+    in_usd = in_usd + cost_matrix['cash'][usd]
 
-    var in_cash = cost_by_type_and_currency['cash'][rub]
-    in_cash = in_cash + cost_by_type_and_currency['cash'][usd]
+    var in_cash = cost_matrix['cash'][rub]
+    in_cash = in_cash + cost_matrix['cash'][usd]
 
-    var in_stocks = cost_by_type_and_currency['stock'][rub]
-    in_stocks = in_stocks + cost_by_type_and_currency['stock'][usd]
+    var in_stocks = cost_matrix['stock'][rub]
+    in_stocks = in_stocks + cost_matrix['stock'][usd]
 
-    var in_bonds = cost_by_type_and_currency['bond'][rub]
-    in_bonds = in_bonds + cost_by_type_and_currency['bond'][usd]
+    var in_bonds = cost_matrix['bond'][rub]
+    in_bonds = in_bonds + cost_matrix['bond'][usd]
 
     var total = in_rubles + in_usd
 
@@ -120,7 +120,7 @@ function compute_agregated(cost_matrix) {
     aggregated['total'] = total
     return aggregated
 }
-function compute_agregated_percentage(aggregated) {
+function compute_aggregated_percentage(aggregated) {
     var percentages = {}
     percentages[rub] = 100 * aggregated[rub] / aggregated['total']
     percentages[usd] = 100 * aggregated[usd] / aggregated['total']
@@ -135,7 +135,12 @@ function create_table_for_document(cost_matrix, aggregated, percentages) {
     var html = `
     <table>
     <tr>
-        <td align="center"> </td>
+        <td align="center">
+            <input id="refresh_PT" type="button" value="Refresh" onclick="compute_and_insert_percentages_table();" >
+                    <script type="text/javascript" src="compute_and_insert.js"></script>
+            </input>
+
+        </td>
         <td align="center">equity_type</td>
         <td align="center">stocks</td>
         <td align="center">bonds</td>
@@ -183,11 +188,34 @@ function add_to_header(el){
     }
 }
 
-var cost_by_type_and_currency = compute_cost_martix()
-var aggregated = compute_agregated(cost_by_type_and_currency)
-var percentages = compute_agregated_percentage(aggregated)
+function compute_and_insert_percentages_table(){
+    var cost_by_type_and_currency = compute_cost_matrix()
+    var aggregated = compute_aggregated(cost_by_type_and_currency)
+    var percentages = compute_aggregated_percentage(aggregated)
 
-var table = create_table_for_document(cost_by_type_and_currency, aggregated, percentages)
-add_to_header(table)
+    var table = create_table_for_document(cost_by_type_and_currency, aggregated, percentages)
+    add_to_header(table)
+}
+
+function proceed_after_header_appear(){
+    header = document.querySelector('[data-qa-file="BrokerAccountsHeaderPure"]')
+    if (header == null){
+        console.log("waiting for a header.")
+        setTimeout(proceed_after_header_appear, 1000)
+    } else {
+        console.log("Header is heare!!!")
+        compute_and_insert_percentages_table()
+    }
+}
+
+function create_empty_table(){
+    var html = '<table><tr><td>refresh</td><td></td></tr></table>'
+    var table = document.createElement('table')
+    table.setAttribute('id', 'percentages_table')
+    table.setAttribute('border', '3')
+    table.setAttribute('width', '100%')
+    table.innerHTML = html
+    add_to_header(table)
+}
 
 
---
+++ load_functions_to_body.js
@@ -0,0 +1,5 @@
+var scriptTag = document.createElement('script');
+scriptTag.src = chrome.extension.getURL('compute_and_insert_functions.js');
+document.getElementsByTagName("head")[0].appendChild(scriptTag);
+document.getElementsByTagName("body")[0].setAttribute("onLoad", "proceed_after_header_appear();");
+// inspired by https://stackoverflow.com/a/31374433
